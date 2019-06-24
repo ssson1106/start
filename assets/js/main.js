b54b43eaf81f5dbc6dad764e5a07ae61083f283d
ssson1106 <ssson1106@gmail.com>
2019-06-24 23:57:17 +0900

    include2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,16 @@
 
 (function($) {
 
+	$(function(){
+		includeLayout();
+	});
+
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)'
-	});
-
-	$(function(){
-		includeLayout();
 	});
 
 	$(function() {
